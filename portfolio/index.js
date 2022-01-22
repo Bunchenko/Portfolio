a58f1ca5580e6d049d4a671f63ba9cf65b7aeb78
Andrei Bunchenko <andrew.by.one@gmail.com>
2022-01-22 21:30:53 +0300

    feat: add burger icon click animation

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,1 +1,7 @@
+const burger = document.querySelector('.burger');
+function toggleBurger() {
+    burger.classList.toggle("change");
+};
+burger.addEventListener('click', toggleBurger);
+
 console.log('Вёрстка валидная +10\nВёрстка семантическая +18\nВёрстка соответствует макету +48\nТребования к css + 10\nИнтерактивность, реализуемая через css +20\nИтого 108')
