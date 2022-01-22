d5358c2da645555456f5e26f56188e94d3f87218
Andrei Bunchenko <andrew.by.one@gmail.com>
2022-01-22 23:04:50 +0300

    feat: add navigation

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,7 +1,21 @@
 const burger = document.querySelector('.burger');
+const navigation = document.querySelector('.navigation-list');
+const navigationMobile = document.querySelector('.navigation-list-mobile');
+const navigationItems = document.querySelectorAll('.navigation-list-item-link');
+
 function toggleBurger() {
     burger.classList.toggle('open');
 };
+
 burger.addEventListener('click', toggleBurger);
+
+burger.addEventListener('click', () => {
+    navigation.classList.toggle('active');
+    if (navigation.classList.contains('active')) {
+        navigationMobile.style.display = 'block';
+    } else {
+        navigationMobile.style.display = 'none';
+    }
+})
 
 console.log('Вёрстка валидная +10\nВёрстка семантическая +18\nВёрстка соответствует макету +48\nТребования к css + 10\nИнтерактивность, реализуемая через css +20\nИтого 108')
