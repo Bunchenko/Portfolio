8ec00883a249519a4f1e8b61943a98a2b7054416
Andrei Bunchenko <andrew.by.one@gmail.com>
2022-01-23 19:23:46 +0300

    feat: add modal window logic

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,21 +1,44 @@
 const burger = document.querySelector('.burger');
 const navigation = document.querySelector('.navigation-list');
 const navigationMobile = document.querySelector('.navigation-list-mobile');
-const navigationItems = document.querySelectorAll('.navigation-list-item-link');
+const navigationMobileItem = document.querySelectorAll('.navigation-list-item-mobile');
 
 function toggleBurger() {
     burger.classList.toggle('open');
 };
 
-burger.addEventListener('click', toggleBurger);
+function windowResize() {
+    if (document.querySelector('html').clientWidth > 768) {
+        navigationMobile.style.display = 'none';
+        burger.classList.remove('open');
+    }
+}
 
-burger.addEventListener('click', () => {
-    navigation.classList.toggle('active');
+function closeModalWindow() {
+    if (burger.classList.contains('open')) {
+        navigation.classList.add('active');
+    } else {
+        navigation.classList.remove('active');
+    }
+
     if (navigation.classList.contains('active')) {
-        navigationMobile.style.display = 'block';
+        navigationMobile.style.display = 'flex';
     } else {
         navigationMobile.style.display = 'none';
     }
-})
+};
+
+navigationMobileItem.forEach(item => item.addEventListener('click', () => {
+    burger.classList.remove('open')
+}));
+
+navigationMobileItem.forEach(item => item.addEventListener('click', closeModalWindow));
+
+burger.addEventListener('click', toggleBurger);
+
+burger.addEventListener('click', closeModalWindow);
+
+window.addEventListener('resize', windowResize);
+
 
 console.log('Вёрстка валидная +10\nВёрстка семантическая +18\nВёрстка соответствует макету +48\nТребования к css + 10\nИнтерактивность, реализуемая через css +20\nИтого 108')
