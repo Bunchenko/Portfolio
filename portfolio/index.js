638fc1e12c4d32c1067ba0f42868a1506c9faa07
Andrei Bunchenko <andrew.by.one@gmail.com>
2022-01-25 23:08:30 +0300

    feat: add language switching

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,3 +1,5 @@
+import i18Obj from './translate.js';
+
 const burger = document.querySelector('.burger');
 const navigation = document.querySelector('.navigation-list');
 const navigationMobile = document.querySelector('.navigation-list-mobile');
@@ -5,15 +7,30 @@ const navigationMobileItem = document.querySelectorAll('.navigation-list-item-mo
 const portfolioButtonsContainer = document.querySelector('.portfolio-buttons');
 const portfolioButtons = document.querySelectorAll('.portfolio-buttons-button');
 const portfolioImages = document.querySelectorAll('.portfolio-images-image');
+const englishButton = document.querySelector('.navigation-container-switch-en');
+const russianButton = document.querySelector('.navigation-container-switch-ru');
 
 function preloadImages() {
     const seasons = ['winter', 'spring', 'summer', 'autumn'];
-    for (item of seasons) {
+    for (let item of seasons) {
         for (let i = 1; i <= 6; i++) {
             const img = new Image();
             img.src = `./assets/img/${item}/${i}.jpg`;
         }
     }
+}
+
+function getTranslate(language) {
+    let translateElements = document.querySelectorAll('[data-i18n]');
+    translateElements.forEach(element => {
+        // if (element.dataset in i18Obj) {
+            // if (element.placeholder) {
+            //     element.placeholder = 
+            //     element.textContent = ''
+            // }
+            element.textContent = i18Obj[language][element.dataset.i18n]
+        // }
+    })
 }
 
 function toggleBurger() {
@@ -57,6 +74,18 @@ function changeImage(event) {
 
 preloadImages();
 
+englishButton.addEventListener('click', () => {
+    russianButton.classList.remove('current');
+    englishButton.classList.add('current');
+    getTranslate('en');
+});
+
+russianButton.addEventListener('click', () => {
+    englishButton.classList.remove('current');
+    russianButton.classList.add('current');
+    getTranslate('ru');
+});
+
 navigationMobileItem.forEach(item => item.addEventListener('click', () => {
     burger.classList.remove('open')
 }));
@@ -70,8 +99,5 @@ burger.addEventListener('click', closeModalWindow);
 window.addEventListener('resize', windowResize);
 
 portfolioButtonsContainer.addEventListener('click', changeImage);
-
-
-
 
 // console.log('Вёрстка соответствует макету +48\nНи на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки + 15\nНа ширине экрана 768рх и меньше реализовано адаптивное меню +22\nИтого 85')
